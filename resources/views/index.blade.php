66e765deab83774dae0455082367d0631ba7c3e3
Nataliia Lazorenko <nataly.lazorenko@speedandfunction.com>
2024-06-11 21:18:32 +0300

    s4-23 Blade directives

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,29 @@
-<div>
-    Hello, I'm a blade template!
-</div>
+<h1>
+    The list of tasks
+</h1>
 
-<!--
-    To access the variable we use double curly braces and $ before the variable name
-    `isset()` - checks if certain variable is defined. It should be closed with `endisset`
--->
-@isset($name)
+<!-- We can use `if` directive and `count` php function to check if tasks array have any elements in it -->
 <div>
-    The name is: {{ $name }}
+    <!-- We can also use: count($tasks) > 0 -->
+    <!-- @if(count($tasks))
+@foreach($tasks as $task)
+            <div>{{ $task->title }}</div>
+@endforeach
+@else
+        <div>There are no tasks!</div>
+@endif-->
+
+    <!--
+        Instead if mixing any other kind of `for` loop with an `if` statement, we can use `forelse` directive
+        `forelse` will iterate aver all the elements in the list, if there are any. It the list is empty, it will display the alternative
+    -->
+    @forelse($tasks as $task)
+        <div>
+            <a
+                href="{{ route('tasks.show', ['id'=>$task->id]) }}">{{ $task->title }}
+            </a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
 </div>
-@endisset
--- routes/web.php
+++ routes/web.php
@@ -13,39 +13,91 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+class Task
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $description,
+        public ?string $long_description,
+        public bool $completed,
+        public string $created_at,
+        public string $updated_at
+    ) {
+    }
+}
+
+$tasks = [
+    new Task(
+        1,
+        'Buy groceries',
+        'Task 1 description',
+        'Task 1 long description',
+        false,
+        '2023-03-01 12:00:00',
+        '2023-03-01 12:00:00'
+    ),
+    new Task(
+        2,
+        'Sell old stuff',
+        'Task 2 description',
+        null,
+        false,
+        '2023-03-02 12:00:00',
+        '2023-03-02 12:00:00'
+    ),
+    new Task(
+        3,
+        'Learn programming',
+        'Task 3 description',
+        'Task 3 long description',
+        true,
+        '2023-03-03 12:00:00',
+        '2023-03-03 12:00:00'
+    ),
+    new Task(
+        4,
+        'Take dogs for a walk',
+        'Task 4 description',
+        null,
+        false,
+        '2023-03-04 12:00:00',
+        '2023-03-04 12:00:00'
+    ),
+];
+
 // To pass data to the template we can use a second argument of the view function
 // It should be an array, keys would be the names of the variables
-Route::get('/', function () {
+Route::get('/', function () use ($tasks) {
     return view('index', [
-        'name' => 'Nataly'
+        'tasks' => $tasks
 
         // If we pass html tags to the variable, they would be escaped by Laravel and displayed as text
         // 'name' => '<b>Nataly</b>' // will be escaped
         // 'name' => '<script></script>' // will be escaped
     ]);
-});
+})->name('tasks.index');
 
-// ->name('route_name') - adds the name to the route
-Route::get('/hello', function () {
-    return 'Hello';
-})->name('hello');
+Route::get('/{id}', function ($id) {
+    return 'One single task';
+})->name('tasks.show');
 
-// Redirects from 'hallo' route to 'hello
-Route::get('/hallo', function () {
-    // return redirect('/hello');
-    return redirect()->route('hello'); // redirects to the named route 'hello'
-});
+// // ->name('route_name') - adds the name to the route
+// Route::get('/hello', function () {
+//     return 'Hello';
+// })->name('hello');
 
-Route::get('/greet/{name}', function ($name) {
-    return 'Hello ' . $name . "!";
-});
+// // Redirects from 'hallo' route to 'hello
+// Route::get('/hallo', function () {
+//     // return redirect('/hello');
+//     return redirect()->route('hello'); // redirects to the named route 'hello'
+// });
+
+// Route::get('/greet/{name}', function ($name) {
+//     return 'Hello ' . $name . "!";
+// });
 
 // Fallback route - all routes that don't exist will redirect to this page 
 Route::fallback(function () {
     return 'Still got somewhere';
 });
-
-// GET
-// POST 
-// PUT 
-// DELETE 
