7de08c1394419d550df5e2925e01e039388fda04
Nataliia Lazorenko <nataly.lazorenko@speedandfunction.com>
2024-06-18 13:51:35 +0300

    s4-29 Model and reading data

--- routes/web.php
+++ routes/web.php
@@ -14,90 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
 // To pass data to the template we can use a second argument of the view function
 // It should be an array, keys would be the names of the variables
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks
-
+        // 'tasks' => \App\Models\Task::all(), // will fetch all the records of a particular model
+        // 'tasks' => \App\Models\Task::latest()->where('completed', true)->get(), // will fetch only completed tasks
+        'tasks' => \App\Models\Task::latest()->get(), // will fetch the most recent tasks first
+        
         // If we pass html tags to the variable, they would be escaped by Laravel and displayed as text
         // 'name' => '<b>Nataly</b>' // will be escaped
         // 'name' => '<script></script>' // will be escaped
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-        /* 
-         * In PHP Arrays are primitive data types, not objects
-         * To be able to interact with arrays using object oriented way, we can use `collect` function - it turns
-         * array into a Laravel's collection object, which lets us call methods (eg. firstWhere)
-         */
-        $task = collect($tasks)->firstWhere('id', $id);
+Route::get('/tasks/{id}', function ($id) {
+    // To get one record from the database table using the model, we refer to the model and call it's method
+    // 'find' - fetches a record from the database by a specific primary key. If it won't find the record in the database, it would return null and we'll get ar error
+    // 'findOrFail' - if it won't find a record by specific primary key ('id' in our case), it will call the abort function with 404 code
 
-        if(!$task) {
-            // `abort` function will just stop the request with a specific response code
-            abort(Response::HTTP_NOT_FOUND);
-        }
-
-        // return view named 'show' and pass $task ti the view
-    return view('show', ['task' => $task]);
+    // return view named 'show' and pass $task to the view
+    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
 
 // // ->name('route_name') - adds the name to the route
