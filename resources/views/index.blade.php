da7b1e5da6f4aded30a77cf4c055228062c99737
Nataliia Lazorenko <nataly.lazorenko@speedandfunction.com>
2024-09-23 23:41:19 +0300

    s4-35 Addig styling with Tailwind css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,15 +3,22 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-<div>
-    <a href="{{ route('tasks.create') }}">Add task!</a>
-</div>
+<nav class="mb-4">
+    <a href="{{ route('tasks.create') }}" class="font-medium text-grey-700 underline decoration-pink-500">
+        Add task!
+    </a>
+</nav>
 
 <!-- Instead if mixing any other kind of `for` loop with an `if` statement, we can use `forelse` directive
 `forelse` will iterate aver all the elements in the list, if there are any. It the list is empty, it will display the alternative -->
 @forelse($tasks as $task)
 <div>
-    <a href="{{ route('tasks.show', ['task'=>$task->id]) }}">{{ $task->title }}
+    <!-- The @class directive simplifies adding conditional classes. You can pass an array of classes:
+    some that are always applied (e.g., font-bold) and others conditionally (e.g., line-through if a task is completed).
+    If the condition is true, the class is added; otherwise, it's not
+    E.g.: @class(['line-through'=> $task->completed]) -->
+    <a href="{{ route('tasks.show', ['task'=>$task->id]) }}"
+        @class(['line-through'=> $task->completed])>{{ $task->title }}
     </a>
 </div>
 @empty
@@ -21,7 +28,7 @@
 <!-- We use an 'if' statement to check if there are any tasks.
 If tasks exist, we display the pagination links by calling the links method on the variable holding the tasks -->
 @if($tasks->count())
-<nav>
+<nav class="mt-4">
     {{ $tasks->links() }}
 </nav>
 @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,18 @@
 
 <head>
     <title>Laravel 10 Task List App</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     @yield('styles')
 </head>
 
-<body>
+<!-- Class 'container' creates a centered container with a max width that adjusts based on the screen size.
+mx-auto horizontally centers the element within the container.
+mt-10 and mb-10 add a margin of 10 units to the top and bottom, respectively. The unit is defined by Tailwind's spacing scale.
+max-w-lg sets the maximum screen size to large, which equals 32 REM or 512 pixels -->
+
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
     <!-- `yield` shows that here we should render the content of particular section  -->
-    <h1>@yield('title')</h1>
+    <h1 class="mb-4 text-2xl">@yield('title')</h1>
     <!-- Here we will render the content of an individual blate template -->
     <div>
         <!-- The best place to display a Flash message is inside the layout, so it's displayed at the top of the page.
@@ -16,7 +22,7 @@
         It has a method has() which lets us check if certain variable exists inside the session.
         To get the value of the variable, we simply call the session function with the name of the variable. -->
         @if(session()->has('success'))
-            <div>{{ session('success') }}</div>
+        <div>{{ session('success') }}</div>
         @endif
 
         @yield('content')
