e036c20bf98a3f712f9487387d074a30983f14b7
Nataliia Lazorenko <nataly.lazorenko@speedandfunction.com>
2024-09-25 13:21:08 +0300

    s4-38 Styling flash message & using Alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
 <head>
     <title>Laravel 10 Task List App</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    {{-- To make the close button functional:
+    1. Add Alpine.js --}}
+    <script src="//unpkg.com/alpinejs" defer></script>
     <!-- To extract a long list of classes into a reusable class, add a style block in our layout file (app.blade.php) below the Tailwind script.
     We need to add comment to prevent the Blade formatter from reformatting the CSS, as it’s not entirely valid CSS and is parsed by the Tailwind Play CDN.
     If we save changes without the comment, the formatter will break it.
@@ -49,13 +52,30 @@ max-w-lg sets the maximum screen size to large, which equals 32 REM or 512 pixel
     <!-- `yield` shows that here we should render the content of particular section  -->
     <h1 class="mb-4 text-2xl">@yield('title')</h1>
     <!-- Here we will render the content of an individual blate template -->
-    <div>
+    {{-- 2. Use the x-data directive to create an Alpine component with an initial flash value of true. This controls whether the flash message is visible --}}
+    <div x-data="{ flash: true }">
         <!-- The best place to display a Flash message is inside the layout, so it's displayed at the top of the page.
         We can use the session() function which is available both in Blade and in Laravel code.
         It has a method has() which lets us check if certain variable exists inside the session.
         To get the value of the variable, we simply call the session function with the name of the variable. -->
         @if(session()->has('success'))
-        <div>{{ session('success') }}</div>
+        {{-- The 'role' attribute improves accessibility by informing screen readers that the element contains important information, like an alert --}}
+        {{-- 3. Add the x-show directive to the flash message <div> to toggle its visibility based on the flash variable --}}
+        <div x-show="flash"
+            class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+            role="alert">
+            <strong class="font-bold">Success!</strong>
+            <div>{{ session('success') }}</div>
+
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                {{-- 4. Add an event listener with @click to the close button (SVG) to set flash to false when clicked --}}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5" @click="flash = false"
+                    stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </span>
+        </div>
         @endif
 
         @yield('content')
